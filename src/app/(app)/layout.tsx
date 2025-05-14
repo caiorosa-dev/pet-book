2f62c3da5c1823ce25897564abeecbe2e7458fcf
André Melo <andregabrielmelo@gmail.com>
2025-05-14 20:18:29 -0300

    Add header

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,16 @@
-import { FullScreenPage } from '@/components/layout/full-screen-page';
+import { FullScreenPage } from "@/components/layout/full-screen-page";
+import { Header } from "@/components/layout/header";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <FullScreenPage inApp className='flex justify-center items-center'>
-      {children}
-    </FullScreenPage>
+    <>
+      <FullScreenPage
+        inApp
+        className="flex flex-col items-center justify-between"
+      >
+        <Header className="flex  items-center justify-between p-4 self-start w-full" />
+        {children}
+      </FullScreenPage>
+    </>
   );
 }
