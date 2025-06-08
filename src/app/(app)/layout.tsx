b771d9e0a745cdbcfaa7463076b8403573766bc8
André Melo <andregabrielmelo@gmail.com>
2025-06-08 12:40:49 -0300

    Adiciona header e navbar do aplicativo

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,13 @@
 import { FullScreenPage } from '@/components/layout/full-screen-page'
+import { Header } from '@/components/layout/header'
+import { Navbar } from '@/components/layout/navbar'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <FullScreenPage inApp className="flex justify-center items-center">
+      <Header className="flex items-center justify-between p-4 self-start w-full" />
       {children}
+      <Navbar className="flex items-center justify-between p-4 self-start w-full" />
     </FullScreenPage>
   )
 }
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,23 @@
+import { MapPin, MessagesSquare } from 'lucide-react'
+
+import { Logo } from '@/components/misc/logo'
+import { Button } from '@/components/ui/button'
+
+export function Header({ className }: { className?: string }) {
+  const currentLocation: string = 'Itajaí, SC'
+
+  return (
+    <div className={className ?? 'flex items-center justify-between p-4'}>
+      <Logo />
+      <div className="flex items-center">
+        <Button className="bg-transparent hover:bg-accent">
+          <MapPin className="text-primary" />
+        </Button>
+        <span className="mx-4">{currentLocation}</span>
+        <Button className="bg-transparent hover:bg-accent">
+          <MessagesSquare className="text-primary" />
+        </Button>
+      </div>
+    </div>
+  )
+}
