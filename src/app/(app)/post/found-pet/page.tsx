2cbc37135aa9b390f3129ee3ec49f082f1f1e136
André Melo <andregabrielmelo@gmail.com>
2025-06-08 19:25:04 -0300

    Adiciona as páginas para criar posts de diferentes tipos

---
+++ src/app/(app)/post/found-pet/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { z } from 'zod'
+
+import { formSchema, PetForm } from './_found-pet-form'
+
+export default function CreatePostPage() {
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    try {
+      // axios({
+      //   method: 'post',
+      //   url: '/post/found-pet/new',
+      //   data: {
+      //     ...values,
+      //   },
+      // })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <div className="max-w-xl w-full mx-auto h-full flex flex-col gap-10">
+        <h1 className="text-3xl font-bold text-center">
+          Informe o pet encontrado
+        </h1>
+        <p className="text-center text-secondary-foreground">
+          Passe as informações como onde viu por último e quando, para ajudar a
+          comunidade a encontrar seu pet.
+        </p>
+        <div className="py-4">
+          <PetForm onSubmit={onSubmit} />
+        </div>
+      </div>
+    </>
+  )
+}
